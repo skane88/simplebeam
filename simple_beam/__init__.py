bf0beb54a5ac934df2eb9b9706c2660ef3bee51b
skane88 <sean.kane@outlook.com.au>
2022-01-17 12:25:01 +1000

    add initial simple_beam module.

---
+++ simple_beam/__init__.py
@@ -0,0 +1,12 @@
+"""
+Basic package for doing beam bending analysis.
+"""
+
+__version__ = "0.0.1"
+
+def main():
+    """
+    Dummy function, will be removed when we get some functionality going.
+    """
+
+    raise NotImplementedError()
