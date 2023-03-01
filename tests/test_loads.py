b65345ea3e03204578d204c1aeba5246d90801d2
skane88 <sean.kane@outlook.com.au>
2023-03-01 10:29:17 +1000

    Add basic test for the UDL load method.

--- tests/test_loads.py
+++ tests/test_loads.py
@@ -4,7 +4,25 @@ Test methods in loads.py
 
 from math import isclose
 
-from simplebeam import cantilever, triangular
+from simplebeam import cantilever, simple, triangular, udl
+
+
+def test_udl():
+    """
+    Test the UDL load helper method
+    """
+
+    length = 4
+    w = 4
+
+    load = udl(magnitude=4)
+
+    beam = simple(length=length, loads=load)
+
+    peak_moment = 0.125 * w * length**2
+
+    assert isclose(beam.moment_at_point(position=length / 2), peak_moment)
+    assert isclose(beam._load_at_point(position=length / 4), w)  # pylint: disable=W0212
 
 
 def test_triangular():
